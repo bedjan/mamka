fe80facdc75525fb05db6caf8bc09067248a0941
bedjan <bedjan@gmail.com>
2017-08-27 07:47:05 +0200

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Vitejte</p>'; ?> 
+ <?php include('https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html'); ?>
+ </body>
+</html>
