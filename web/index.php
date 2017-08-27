48be5d8c28a185fef6ce433bbf78c2212eaf7175
bedjan <bedjan@gmail.com>
2017-08-27 09:41:58 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,17 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
- <head>
-  <title>PHP Test</title>
+<head>
+<title>Advokátní Kancelář JUDr. Zdenky Bednářové v Rychnově n.K.</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
  </head>
  <body>
- <?php echo '<p>Vitejte</p>'; ?> 
-  <?php include "https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html"; ?>
-  <?php echo '<p>Vitejte 1</p>'; ?> 
- <?php include('https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html'); ?>
-  <p>vlozeno</p>
-  <?php
-echo '<a href="https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html">Home</a> -
-<a href="https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html">PHP Tutorial</a>';
+
+<!-- stazeni souboru -->
+<?php error_reporting(E_ALL);?>
+
+<?php
+
+ $source = "https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html";
+ $destination = "obsah.html";
+
+ $data = file_get_contents($source);
+ $file = fopen($destination, "w+");
+ fputs($file, $data);
+ fclose($file);
+
 ?>
-  
+
+<!-- pripadne vytisteni znaku primo <?php echo '<p>Vitejte</p>'; ?> -->
+
+<!-- zobrazeni souboru -->
+<?php require ('obsah.html'); ?>
+
+
+
  </body>
 </html>
+
+
+
