f29962570261e94a72b277d1d234ef3d3122a67b
bedjan <bedjan@gmail.com>
2017-08-27 07:53:38 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,5 +5,11 @@
  <body>
  <?php echo '<p>Vitejte</p>'; ?> 
  <?php include('https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html'); ?>
+  <p>vlozeno</p>
+  <?php
+echo '<a href="https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html">Home</a> -
+<a href="https://raw.githubusercontent.com/bedjan/mamka/master/web/obsah.html">PHP Tutorial</a>';
+?>
+  
  </body>
 </html>
